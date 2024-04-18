4c2685f5daf9b788397778eb2c7344d242cb86c2
Joel Alves <166664754+JoelAlvess@users.noreply.github.com>
2024-04-18 20:24:58 -0300

    Estudando

---
+++ aula05/aula05.js
@@ -0,0 +1,7 @@
+var vel = 145
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if(vel > 100){
+    console.log(`Voce ultrapassou a velocidade e foi multado`)
+}
+console.log(`Dirija com segurança`)
---
+++ aula05/exercicio.06.js
@@ -0,0 +1,7 @@
+var pais = "Japao" 
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log(`Você é brasileeiro`)
+}else{
+    console.log(`Você é estrangeiro`)
+}
