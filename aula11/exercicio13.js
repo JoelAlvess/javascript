f8ea70bbc680cad4ef7f0c30d79473458b58d8ff
Joel Alves <166664754+JoelAlvess@users.noreply.github.com>
2024-04-25 18:35:56 -0300

    Meu primeiro relogio

--- aula11/exercicio13.js
+++ aula11/exercicio13.js
@@ -1,28 +1,31 @@
 var now = new Date()
 var weekDay = now.getDay()
-var hours = new Date
+var hours = new Date()
 var hours = hours.getHours()
+var min = new Date()
+var min = min.getMinutes()
+
 switch(weekDay) {
     case 0:
-        console.log(`Domingo, ${hours}`)
+        console.log(`Domingo, ${hours}:${min}`)
         break
     case 1: 
-         console.log(`Segunda-Feira`)
+         console.log(`Segunda-Feira ${hours}:${min}`)
          break
     case 2: 
-         console.log(`Terça-Feira`)
+         console.log(`Terça-Feira ${hours}:${min}`)
          break
     case 3: 
-         console.log(`Quarta-Feira`)
+         console.log(`Quarta-Feira ${hours}:${min}`)
          break
     case 4: 
-         console.log(`Quinta-Feira, ${hours}`)
+         console.log(`Quinta-Feira, ${hours}:${min}`)
          break
     case 5: 
-         console.log(`Sexta-Feira`)
+         console.log(`Sexta-Feira ${hours}:${min}`)
          break
     case 6: 
-         console.log(`Sábado`)
+         console.log(`Sábado ${hours}`)
          break
     default:
         console.log(`Esse dia não existe`)
